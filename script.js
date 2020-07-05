59bfad60cad0ec4914cc9f5e90c4717dffab1544
GuiHakime <65927354+GuiHakime@users.noreply.github.com>
2020-07-05 15:56:59 -0300

    V3

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ let menu = document.querySelector(".menu")
 iconMenu.addEventListener('click', () => {
     menu.classList.toggle('menu__move')
 });
+
+//Calendário 
+
+const lang = navigator.language
+let date = new Date()
+
+let dayNumber = date.getDate()
+let month = date.getMonth()
+let dayName = date.toLocaleString(lang, {
+    weekday: 'long'
+})
+let monthName = date.toLocaleString(lang, {
+    month: 'long'
+})
+let year = date.getFullYear()
+
+document.getElementById('mes').innerHTML = monthName
+document.getElementById('dia').innerHTML = dayName
+document.getElementById('numero-dia').innerHTML = dayNumber
+document.getElementById('ano').innerHTML = year
